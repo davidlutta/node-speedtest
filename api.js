760f19a1a7c85b1673d5c9f577303c7e67d22f1a
Davidlutta <davidlutta2010@gmail.com>
2021-10-22 13:00:59 +0300

    Add path to chromium browser

--- api.js
+++ api.js
@@ -46,7 +46,7 @@ module.exports = options => (
 	new Observable(observer => {
 		// Wrapped in async IIFE as `new Observable` can't handle async function
 		(async () => {
-			const browser = await puppeteer.launch({args: ['--no-sandbox']});
+			const browser = await puppeteer.launch({headless:true, args: ['--no-sandbox'], executablePath:"/usr/bin/chromium-browser"});
 			const page = await browser.newPage();
 			await page.goto('https://fast.com');
 			await init(browser, page, observer, options);
