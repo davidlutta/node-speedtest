848937ee0f0d28dc26ad708d9cf5cbc318e2c0d3
David Lutta <davidlutta2010@gmail.com>
2022-07-12 21:52:56 +0300

    Move index file into src folder

--- src/api.js
+++ src/api.js
@@ -46,7 +46,8 @@ module.exports = options => (
 	new Observable(observer => {
 		// Wrapped in async IIFE as `new Observable` can't handle async function
 		(async () => {
-			const browser = await puppeteer.launch({headless:true, args: ['--no-sandbox'], executablePath:"/usr/bin/chromium-browser"});
+			const browser = await puppeteer.launch({headless:true, args: ['--no-sandbox', '--disable-gpu', '--disable-setuid-sandbox', '--disable-dev-shm-usage']
+		});
 			const page = await browser.newPage();
 			await page.goto('https://fast.com');
 			await init(browser, page, observer, options);
---
+++ src/index.js
@@ -0,0 +1,66 @@
+const api = require('./api.js');
+const { logger } = require('./util/logger.js');
+const { influx } = require('./util/influx.js'); 
+const CronJob = require('cron').CronJob;
+
+//cron job schedule to run every 15 minutes.
+let CRON_SCHEDULE = '*/15 * * * *';
+
+// checking if the database already exists if not we create it.
+influx.getDatabaseNames()
+.then(names => {
+    if (!names.includes('internetspeed')) {
+        logger.info("Creating the database")
+        return influx.createDatabase('internetspeed');
+    }
+}).catch(err => logg(`Error fetching DBs: ${err}`));
+
+/**
+ * This method writes the points to the database.
+ * @param {number} downloadSpeed download speed from test
+ * @param {number} uploadSpeed the upload speed from the test
+ * @param {number} ping the ping from the test
+ */
+function writePoints(downloadSpeed, uploadSpeed, ping) {
+    influx.writePoints([
+        {
+            measurement: 'internetspeed',
+            tags: { host: "DavidEzekiel"},
+            fields: {
+                downloadSpeed: downloadSpeed,
+                uploadSpeed: uploadSpeed,
+                ping: ping
+            }
+        }
+    ]).then(res =>{
+        logger.info("Written to DB")
+    }).catch(err => {
+        logger.error(`Error saving data to influxDB: ${err}`);
+    });
+}
+
+// Main application function
+function main() {
+    api().forEach(
+        result =>{
+            if(result.isDone){
+                writePoints(downloadSpeed=result.downloadSpeed,
+                    uploadSpeed=result.uploadSpeed,
+                    ping=result.latency);
+            }
+        }
+    );
+}
+
+let job = new CronJob(
+    CRON_SCHEDULE,
+    main,
+    function onComplete() {
+        logger.info(`Completed Cron Job`);
+    },
+    false,
+    'Africa/Nairobi'
+)
+
+// Starting job
+job.start();
